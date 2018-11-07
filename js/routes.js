96b66a950199ba6000911f7bdeae2e6c9e068ff3
meital pinhas konon <pinhas.meital@gmail.com>
2018-11-07 15:21:11 +0200

    all working meital

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 
 import appSus from './pages/home.cmp.js'
 import missKeep from './pages/miss-keep.cmp.js'
-import misterEmail from './pages/mister-email.cmp.js'
+import misterEmail from './pages/email-app.cmp.js'
 
 const routes = [
     {path: '/', component: appSus},
